54c56d846b30775e7643d7633c1e76b452637241
Kendall Martinez <kendallmartinezcarvajal4@gmail.com>
2024-11-19 23:05:33 -0600

    acts

--- Cliente.c
+++ Cliente.c
@@ -31,5 +31,28 @@ int main() {
 
     printf("Combinacion escrita exitosamente en el archivo '%s'.\n", filename);
 
+    sleep(30);
+
+    const char *resultao = "resultados.txt";
+
+    FILE* result = fopen(resultao, "r");
+    if (result == NULL){
+        printf("No se encontro el archivo combinacion.txt \n");
+    }
+    char combi_str[1024];
+    if (fgets(combi_str, sizeof(combi_str), result)!= NULL){
+        combi_str[strcspn(combi_str, "\n")] = '\0';
+        printf("Resultado leido: %s \n", combi_str);
+        enviarComando(serial_port,combi_str);
+    } else {
+        printf("Error al leer el archivo\n");
+    }
+
+    // Cerrar el archivo
+    fclose(result);
+    cerrarComunicacion(serial_port);
+
+    printf("Combinacion leida exitosamente del archivo '%s'.\n", filename);
+
     return EXIT_SUCCESS;
 }
